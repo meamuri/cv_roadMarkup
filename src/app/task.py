cd267e2d9b8eb228a7f8576d6f2c446fde3b6e77
meamuri <meammf@yandex.ru>
2017-07-02 18:37:44 +0300

    for foreign help

--- src/app/task.py
+++ src/app/task.py
@@ -2,7 +2,7 @@ from __future__ import division
 from __future__ import print_function
 
 from sklearn import linear_model
-from skimage import data
+from skimage import data, io
 from sklearn import model_selection
 from sklearn.model_selection import train_test_split
 from sklearn import svm
@@ -15,7 +15,159 @@ import os
 
 
 def make_network():
-    return linear_model.LinearRegression()
+    return get_reg()
+    # return linear_model.LinearRegression()
+
+
+def get_images():
+    # import numpy as np
+    path = './output/markup'
+    m_up_len = len([path + name for name in os.listdir(path)
+                    if os.path.isfile(path + name)])
+    path = './output/unmarkup'
+    un_m_up_len = len([path + name for name in os.listdir(path)
+                       if os.path.isfile(path + name)])
+    obj = {
+        'arrays': io.imread_collection('./output/markup/*.jpg:./output/markup/*.jpg').concatenate(),
+        'len_mark': m_up_len,
+        'len_unmark': un_m_up_len,
+    }
+    return obj
+
+
+def get_reg():
+    obj = get_images()
+    X = obj['arrays']
+
+    y = [1] * obj['len_mark'] + [0] * obj['len_unmark']
+    x_train, x_test, y_train, y_test = model_selection.train_test_split(X,
+                                                                        y, test_size=0.20)
+
+    reg = linear_model.LinearRegression()
+
+    reg.fit(X=x_train, y=y_train)
+
+    return reg
+
+    
+def smthng_that_doesnt_work(training_path_a='markup/', training_path_b='unmarkup/', print_metrics=True):
+    """
+    Trains a classifier. training_path_a and training_path_b should be
+    directory paths and each of them should not be a subdirectory of the other
+    one. training_path_a and training_path_b are processed by
+    process_directory().
+
+    Args:
+      training_path_a (str): directory containing sample images of class A.
+      training_path_b (str): directory containing sample images of class B.
+      print_metrics  (boolean, optional): if True, print statistics about
+        classifier performance.
+
+    Returns:
+      A classifier (sklearn.svm.SVC).
+    """
+    training_a = process_directory('output/' + training_path_a)
+    training_b = process_directory('output/' + training_path_b)
+    # training_a = get_images(training_path_a)
+    # training_b = get_images(training_path_b)
+
+    # data contains all the training data (a list of feature vectors)
+    the_data = training_a + training_b
+    # target is the list of target classes for each feature vector: a '1' for
+    # class A and '0' for class B
+    target = [1] * len(training_a) + [0] * len(training_b)
+    # split training data in a train set and a test set. The test set will
+    # contain 20% of the total
+    x_train, x_test, y_train, y_test = model_selection.train_test_split(the_data,
+                                                                        target, test_size=0.20)
+    # define the parameter search space
+    parameters = {'kernel': ['linear', 'rbf'], 'C': [1, 10, 100, 1000],
+                  'gamma': [0.01, 0.001, 0.0001]}
+    # search for the best classifier within the search space and return it
+    clf = model_selection.GridSearchCV(svm.SVC(C=1), parameters).fit(x_train, y_train)
+    classifier = clf.best_estimator_
+    if print_metrics:
+        print()
+        print('Parameters:', clf.best_params_)
+        print()
+        print('Best classifier score')
+        print(metrics.classification_report(y_test,
+                                            classifier.predict(x_test)))
+    return classifier
+
+
+def train():
+    mup = get_images('markup/')
+    umup = get_images('unmarkup/')
+    X = mup + umup
+
+    # To apply an classifier on this data, we need to flatten the image, to
+    # turn the data in a (samples, feature) matrix:
+
+    # n_samples = len(digits.images)
+    # X = digits.images.reshape((n_samples, -1))
+    # y = digits.target
+    y = [1] * len(mup) + [0] * len(umup)
+    # Split the dataset in two equal parts
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.5, random_state=0)
+
+    # Set the parameters by cross-validation
+    tuned_parameters = [{'kernel': ['rbf'], 'gamma': [1e-3, 1e-4],
+                         'C': [1, 10, 100, 1000]},
+                        {'kernel': ['linear'], 'C': [1, 10, 100, 1000]}]
+
+    scores = ['precision', 'recall']
+
+    for score in scores:
+        print("# Tuning hyper-parameters for %s" % score)
+        print()
+
+        clf = model_selection.GridSearchCV(svm.SVC(C=1), tuned_parameters, cv=5,
+                                           scoring='%s_macro' % score)
+        clf.fit(X_train, y_train)
+
+        print("Best parameters set found on development set:")
+        print()
+        print(clf.best_params_)
+        print()
+        print("Grid scores on development set:")
+        print()
+        means = clf.cv_results_['mean_test_score']
+        stds = clf.cv_results_['std_test_score']
+        for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+            print("%0.3f (+/-%0.03f) for %r"
+                  % (mean, std * 2, params))
+        print()
+
+        print("Detailed classification report:")
+        print()
+        print("The model is trained on the full development set.")
+        print("The scores are computed on the full evaluation set.")
+        print()
+        y_true, y_pred = y_test, clf.predict(X_test)
+        print(classification_report(y_true, y_pred))
+        print()
+
+
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
 
 
 def process_directory(directory):
@@ -73,40 +225,28 @@ def process_image(the_image, blocks=4):
     feature = [0] * blocks * blocks * blocks
     pixel_count = 0
     for pixel in the_image.getdata():
-        ridx = int(pixel[0]/(256/blocks))
-        gidx = int(pixel[1]/(256/blocks))
-        bidx = int(pixel[2]/(256/blocks))
+        ridx = int(pixel[0] / (256 / blocks))
+        gidx = int(pixel[1] / (256 / blocks))
+        bidx = int(pixel[2] / (256 / blocks))
         idx = ridx + gidx * blocks + bidx * blocks * blocks
         feature[idx] += 1
         pixel_count += 1
-    return [x/pixel_count for x in feature]
+    return [x / pixel_count for x in feature]
 
 
-def get_images(path):
-    path = './output/' + path
-    names = [path + name for name in os.listdir(path)
-             if os.path.isfile(path + name)]
-    arrays = [data.imread(name) for name in names]
-
-    return arrays
-
-
-def get_coefs(arrays):
-    training_a = get_images('markup/')  # process_directory('output/' + training_path_a)
-    training_b = get_images('unmarkup/')  # process_directory('output/' + training_path_b)
-    # data contains all the training data (a list of feature vectors)
-    the_data = training_a + training_b
-    # target is the list of target classes for each feature vector: a '1' for
-    # class A and '0' for class B
-    target = [1] * len(training_a) + [0] * len(training_b)
-    x_train, x_test, y_train, y_test = model_selection.train_test_split(the_data,
-                                                                        target, test_size=0.20)
-
-    reg = linear_model.LinearRegression()
-
-    reg.fit(X=x_train, y=y_train)
-
-    return reg.coef_
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
 
 
 def bin_objects(X, y):
@@ -122,104 +262,3 @@ def bin_objects(X, y):
     # summarize the fit of the model
     print(metrics.classification_report(expected, predicted))
     print(metrics.confusion_matrix(expected, predicted))
-
-
-def smthng_that_doesnt_work(training_path_a='markup/', training_path_b='unmarkup/', print_metrics=True):
-    """
-    Trains a classifier. training_path_a and training_path_b should be
-    directory paths and each of them should not be a subdirectory of the other
-    one. training_path_a and training_path_b are processed by
-    process_directory().
-
-    Args:
-      training_path_a (str): directory containing sample images of class A.
-      training_path_b (str): directory containing sample images of class B.
-      print_metrics  (boolean, optional): if True, print statistics about
-        classifier performance.
-
-    Returns:
-      A classifier (sklearn.svm.SVC).
-    """
-    training_a = process_directory('output/' + training_path_a)
-    training_b = process_directory('output/' + training_path_b)
-    # training_a = get_images(training_path_a)
-    # training_b = get_images(training_path_b)
-
-    # data contains all the training data (a list of feature vectors)
-    the_data = training_a + training_b
-    # target is the list of target classes for each feature vector: a '1' for
-    # class A and '0' for class B
-    target = [1] * len(training_a) + [0] * len(training_b)
-    # split training data in a train set and a test set. The test set will
-    # contain 20% of the total
-    x_train, x_test, y_train, y_test = model_selection.train_test_split(the_data,
-                                                                        target, test_size=0.20)
-    # define the parameter search space
-    parameters = {'kernel': ['linear', 'rbf'], 'C': [1, 10, 100, 1000],
-                  'gamma': [0.01, 0.001, 0.0001]}
-    # search for the best classifier within the search space and return it
-    clf = model_selection.GridSearchCV(svm.SVC(C=1), parameters).fit(x_train, y_train)
-    classifier = clf.best_estimator_
-    if print_metrics:
-        print()
-        print('Parameters:', clf.best_params_)
-        print()
-        print('Best classifier score')
-        print(metrics.classification_report(y_test,
-                                            classifier.predict(x_test)))
-    return classifier
-
-
-def train():
-    # Loading the Digits dataset
-    mup = get_images('markup/')
-    umup = get_images('unmarkup/')
-    X = mup + umup
-
-    # To apply an classifier on this data, we need to flatten the image, to
-    # turn the data in a (samples, feature) matrix:
-
-    # n_samples = len(digits.images)
-    # X = digits.images.reshape((n_samples, -1))
-    # y = digits.target
-    y = [1] * len(mup) + [0] * len(umup)
-    # Split the dataset in two equal parts
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.5, random_state=0)
-
-    # Set the parameters by cross-validation
-    tuned_parameters = [{'kernel': ['rbf'], 'gamma': [1e-3, 1e-4],
-                         'C': [1, 10, 100, 1000]},
-                        {'kernel': ['linear'], 'C': [1, 10, 100, 1000]}]
-
-    scores = ['precision', 'recall']
-
-    for score in scores:
-        print("# Tuning hyper-parameters for %s" % score)
-        print()
-
-        clf = model_selection.GridSearchCV(svm.SVC(C=1), tuned_parameters, cv=5,
-                           scoring='%s_macro' % score)
-        clf.fit(X_train, y_train)
-
-        print("Best parameters set found on development set:")
-        print()
-        print(clf.best_params_)
-        print()
-        print("Grid scores on development set:")
-        print()
-        means = clf.cv_results_['mean_test_score']
-        stds = clf.cv_results_['std_test_score']
-        for mean, std, params in zip(means, stds, clf.cv_results_['params']):
-            print("%0.3f (+/-%0.03f) for %r"
-                  % (mean, std * 2, params))
-        print()
-
-        print("Detailed classification report:")
-        print()
-        print("The model is trained on the full development set.")
-        print("The scores are computed on the full evaluation set.")
-        print()
-        y_true, y_pred = y_test, clf.predict(X_test)
-        print(classification_report(y_true, y_pred))
-        print()
