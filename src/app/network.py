e766c0791365b571be149a9ff5cb1a154b77c2ef
meamuri <meammf@yandex.ru>
2017-07-02 16:21:33 +0300

    random for plt figure image select

--- src/app/network.py
+++ src/app/network.py
@@ -1,58 +1,72 @@
 from __future__ import print_function
-import numpy as np
-import matplotlib.pyplot as plt
 
-from skimage.transform import rotate
-from skimage.feature import local_binary_pattern
+import matplotlib.pyplot as plt
+import numpy as np
 from skimage import data
+from skimage.feature import local_binary_pattern
+from skimage.transform import rotate
 
 # settings for LBP
 RADIUS = 2
 N_POINTS = 8 * RADIUS
-
-
-def hist(ax, lbp):
-    n_bins = int(lbp.max() + 1)
-    return ax.hist(lbp.ravel(), normed=True, bins=n_bins, range=(0, n_bins),
-                   facecolor='0.5')
-
-
 METHOD = 'uniform'
-plt.rcParams['font.size'] = 9
 
 
-def kullback_leibler_divergence(p, q):
-    p = np.asarray(p)
-    q = np.asarray(q)
-    filt = np.logical_and(p != 0, q != 0)
-    return np.sum(p[filt] * np.log2(p[filt] / q[filt]))
+def show_graphics():
+    """
+    Функция позволяет продемонстрировать корреляцию гистограмм изображений
+    с разметкой и без нее.
+    :return: None
+    """
+    def hist(ax, lbp):
+        n_bins = int(lbp.max() + 1)
+        return ax.hist(lbp.ravel(), normed=True, bins=n_bins, range=(0, n_bins),
+                       facecolor='0.5')
 
+    plt.rcParams['font.size'] = 9
 
-def match(refs, img):
-    best_score = 10
-    best_name = None
-    lbp = local_binary_pattern(img, N_POINTS, RADIUS, METHOD)
-    n_bins = int(lbp.max() + 1)
-    hist, _ = np.histogram(lbp, normed=True, bins=n_bins, range=(0, n_bins))
-    for name, ref in refs.items():
-        ref_hist, _ = np.histogram(ref, normed=True, bins=n_bins,
-                                   range=(0, n_bins))
-        score = kullback_leibler_divergence(hist, ref_hist)
-        if score < best_score:
-            best_score = score
-            best_name = name
-    return best_name
+    def divergence(p, q):
+        p = np.asarray(p)
+        q = np.asarray(q)
+        filt = np.logical_and(p != 0, q != 0)
+        return np.sum(p[filt] * np.log2(p[filt] / q[filt]))
 
+    def match(refs, img):
+        best_score = 10
+        best_name = None
+        lbp = local_binary_pattern(img, N_POINTS, RADIUS, METHOD)
+        n_bins = int(lbp.max() + 1)
+        hist, _ = np.histogram(lbp, normed=True, bins=n_bins, range=(0, n_bins))
+        for name, ref in refs.items():
+            ref_hist, _ = np.histogram(ref, normed=True, bins=n_bins,
+                                       range=(0, n_bins))
+            score = divergence(hist, ref_hist)
+            if score < best_score:
+                best_score = score
+                best_name = name
+        return best_name
 
-def just_do_it():
-    markup = data.imread('./output/markup/markup-3.jpg')
-    unmarkup = data.imread('./output/unmarkup/unmarkup-5.jpg')
-    # wall = data.load('rough-wall.png')
+    def read_rand_pic_from_folder(folder):
+        """
+        выбирает случайную фотографию из предложенной папки 'markup/' или 'unmarkup/'
+        :param folder:
+        :return:
+        """
+        import random
+        import os
+        path = './output/' + folder
+        cnt = len([name for name in os.listdir(path)
+                   if os.path.isfile(os.path.join(path, name))])
+        dig = random.randint(0, cnt - 1)
+        path += folder[0:-1] + '-' + str(dig) + '.jpg'
+        return data.imread(path)
+
+    markup = read_rand_pic_from_folder('markup/')
+    unmarkup = read_rand_pic_from_folder('unmarkup/')
 
     refs = {
-        'brick': local_binary_pattern(markup, N_POINTS, RADIUS, METHOD),
-        'grass': local_binary_pattern(unmarkup, N_POINTS, RADIUS, METHOD),
-        # 'wall': local_binary_pattern(wall, N_POINTS, RADIUS, METHOD)
+        'markup': local_binary_pattern(markup, N_POINTS, RADIUS, METHOD),
+        'unmarkup': local_binary_pattern(unmarkup, N_POINTS, RADIUS, METHOD),
     }
 
     # classify rotated textures
@@ -71,16 +85,12 @@ def just_do_it():
 
     ax1.imshow(markup)
     ax1.axis('off')
-    hist(ax4, refs['brick'])
+    hist(ax4, refs['markup'])
     ax4.set_ylabel('Percentage')
 
     ax2.imshow(unmarkup)
     ax2.axis('off')
-    hist(ax5, refs['grass'])
+    hist(ax5, refs['unmarkup'])
     ax5.set_xlabel('Uniform LBP values')
-
-    # ax3.imshow(wall)
-    # ax3.axis('off')
-    # hist(ax6, refs['wall'])
 
     plt.show()
--- src/application.py
+++ src/application.py
@@ -1,9 +1,14 @@
 from .dialog import *
 from .app.init import make_model_from_dataset
 from .app.utils import create_dataset
+from .app.network import show_graphics
 
 
 def general_app():
+    """
+    Программа, осуществляющая подготовку всех данных для работы с нейросетью
+    :return: None
+    """
     while True:
         print_main_menu()
         scan = input("Введите пункт меню -> ")
@@ -23,12 +28,20 @@ def general_app():
 
 
 def next_app():
+    """
+    Программа, позволяющая взаимодействовать с обученной нейросетью
+    :return: None
+    """
     while True:
         print_sub_menu()
         scan = input("Введите пункт меню -> ")
-        action = input_to_digit(user_input=scan, item_max=1)
+        action = input_to_digit(user_input=scan, item_max=3)
         if action == 1:  # обработать
             pass
+        if action == 2:  # иллюстрировать
+            show_graphics()
+        if action == 3:  # справка
+            print_help_for_sub()
         elif action == 0:
             return
         else:
@@ -36,9 +49,12 @@ def next_app():
 
 
 def run():
+    """
+    Программа, вызываемая из точки входа приложения
+    :return: None
+    """
     if general_app():
         print("\nНейросеть успешно обучена!")
         print("\nПриложение по распознаванию дорожной разметки готово и запущено")
         next_app()
     print("\nПрограмма завершила работу!")
-
